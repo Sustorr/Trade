908a80f53625b94e2562144883a035c087b901dd
Sustorr <89983676+Sustorr@users.noreply.github.com>
2022-04-20 23:11:53 +0100

    Update Settings.lua

--- Settings.lua
+++ Settings.lua
@@ -1,18 +1,18 @@
-_G.WhoToTrade = "";
-_G.AmountToTake = nil;
+_G.WhoToTrade = "iChewKot";
+_G.AmountToTake = 6;
  
-_G.Item1Name = "";
-_G.Item2Name = "";
-_G.Item3Name = "";
-_G.Item4Name = "";
-_G.Item5Name = "";
-_G.Item6Name = "";
+_G.Item1Name = "Twisted";
+_G.Item2Name = "Dreamwalker";
+_G.Item3Name = "Rainbow Saber";
+_G.Item4Name = "Elegant Saber";
+_G.Item5Name = "Seraph";
+_G.Item6Name = "Vortex";
  
-_G.Item1Amount = "";
-_G.Item2Amount = "";
-_G.Item3Amount = "";
-_G.Item4Amount = "";
-_G.Item5Amount = "";
-_G.Item6Amount = "";
+_G.Item1Amount = "1";
+_G.Item2Amount = "1";
+_G.Item3Amount = "1";
+_G.Item4Amount = "1";
+_G.Item5Amount = "1";
+_G.Item6Amount = "1";
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Sustorr/Trade/main/Yeah.lua", true))();
