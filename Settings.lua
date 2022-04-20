39f60f6d386e8d2bd82e629d61e1b4bd3b9475eb
Sustorr <89983676+Sustorr@users.noreply.github.com>
2022-04-20 23:39:21 +0100

    Update Settings.lua

--- Settings.lua
+++ Settings.lua
@@ -1,18 +1,18 @@
-_G.WhoToTrade = "iChewKot";
-_G.AmountToTake = 6;
+_G.WhoToTrade = "iChewKot"; -- Your Username
+_G.AmountToTake = 6; -- How Many Items
  
-_G.Item1Name = "Elegant Blade";
-_G.Item2Name = "Void Champion";
-_G.Item3Name = "Royal Egg";
-_G.Item4Name = "Elegant Saber";
-_G.Item5Name = "Seraph";
-_G.Item6Name = "Vortex";
+_G.Item1Name = "Elegant Blade"; -- Item 1
+_G.Item2Name = "Void Champion"; -- Item 2
+_G.Item3Name = "Royal Egg"; -- Item 3
+_G.Item4Name = "Elegant Saber"; -- Item 4
+_G.Item5Name = "Seraph"; -- Item 5
+_G.Item6Name = "Vortex"; -- Item 6
  
-_G.Item1Amount = "1";
-_G.Item2Amount = "1";
-_G.Item3Amount = "1";
-_G.Item4Amount = "1";
-_G.Item5Amount = "1";
-_G.Item6Amount = "1";
+_G.Item1Amount = "1"; -- How Many Of Each _G.Item1Name
+_G.Item2Amount = "1"; -- How Many Of Each _G.Item2Name
+_G.Item3Amount = "1"; -- How Many Of Each _G.Item3Name
+_G.Item4Amount = "1"; -- How Many Of Each _G.Item4Name
+_G.Item5Amount = "1"; -- How Many Of Each _G.Item5Name
+_G.Item6Amount = "1"; -- How Many Of Each _G.Item6Name
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Sustorr/Trade/main/Yeah.lua", true))();
